5b13155ba17bd638d06fc651046441512f71df63
Krishna9029 <143097309+Krishna9029@users.noreply.github.com>
2025-07-27 19:15:08 +0530

    git push

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,42 +8,44 @@ import Footer from '../Components/Footer'
 import About from '../Components/About'
 
 const Home = () => {
-      const [dark, setDark] = useState(() => {
-        if (typeof window !== 'undefined') {
-          return (
-            localStorage.theme === 'dark' ||
-            (!('theme' in localStorage) &&
-              window.matchMedia('(prefers-color-scheme: dark)').matches)
-          );
-        }
-        return false;
-      })
-      useEffect(() => {
-        const root = window.document.documentElement;
-        if (dark) {
-          root.classList.add('dark');
-          localStorage.setItem('theme', 'dark');
-        } else {
-          root.classList.remove('dark');
-          localStorage.setItem('theme', 'light');
-        }
-      }, [dark]);
-    
-      const toggleDarkMode = () => {
-        setDark((prev) => !prev);
-      };
+  const [dark, setDark] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return (
+        localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches)
+      );
+    }
+    return false;
+  })
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (dark) {
+      root.classList.remove('light');
+      root.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      root.classList.remove('dark');
+      root.classList.add('light');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [dark]);
 
-    return (
-        <div className="dark:bg-gray-900 bg-white text-gray-800 dark:text-gray-200 transition-colors ">
-            <Navbar darkMode={dark} toggleDarkMode={toggleDarkMode} />
-            <Hero />
-            <Skills />
-            <Projects />
-            <About/>
-            <Contact />
-            <Footer/>
-        </div>
-    )
+  const toggleDarkMode = () => {
+    setDark((prev) => !prev);
+  };
+
+  return (
+    <div className="dark:bg-gray-900 bg-white text-gray-800 dark:text-gray-200 transition-colors ">
+      <Navbar darkMode={dark} toggleDarkMode={toggleDarkMode} />
+      <Hero />
+      <Skills />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  )
 }
 
 export default Home
